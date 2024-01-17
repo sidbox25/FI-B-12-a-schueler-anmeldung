feb65c3eb2a8712ffd125dfbfffb71d69af4e220
Hussen Jamoul <jamoulh312@gmail.com>
2024-01-17 05:17:54 +0100

    [ADD] Homepage with global header and body

--- index.php
+++ index.php
@@ -2,26 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=yes">
     <title>Schueler Anmeldung vervaltungs software</title>
-    <link rel="stylesheet" href="src/StudentPersonalData/View/studentPersonalDataStyles.css">
+    <link rel="stylesheet" href="/src/Main/View/assets/main.layout.css">
 </head>
 <body>
-    <p> u are stoopid hon hon hon(mocks u in french)</p>
     <?php
-
 
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     include 'autoload.php';
     include "vendor/autoload.php";
 
+    use src\GeschafftTab\Controller\GeschafftTabController;
+    use src\HomePage\HomePageController\HomePageController;
     use src\StudentPersonalData\Controller\StudentPersonalDataController;
     use src\StudentRegistration\Controller\StudentRegistrationController;
     use src\StudentRegistration\Controller\StudentSchoolVisitsController;
     use src\Core\Connector;
 
-    if($_SERVER['REQUEST_URI'] === '/foo') {
+    if ($_SERVER['REQUEST_URI'] === '/') {
+        $homePageController = new HomePageController();
+        $homePageController->showAction();
+    }
+
+    if ($_SERVER['REQUEST_URI'] === '/foo') {
         $studentRegistrationController = new StudentRegistrationController();
         $studentRegistrationController->StudentRegistrationViewAction();
     }
@@ -32,7 +37,7 @@
         $name = "sidney";
         $stm->bindParam(':name',$name);
         $stm->execute();
-    } 
+    }
     if($_SERVER['REQUEST_URI'] === '/schulbesuch') {
         $studentSchoolVisitsController = new StudentSchoolVisitsController();
         $studentSchoolVisitsController->showStudentSchoolVisitsAction();
@@ -48,7 +53,5 @@
 
 
     ?>
-
 </body>
-
 </html>
---
+++ src/HomePage/HomePageView/HomePageView.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace src\HomePage\HomePageView;
+
+class HomePageView
+{
+
+    public function render()
+    {
+        echo '
+            <header>
+                <img id="logo" src="src/Main/View/assets/logo.png"">
+                <h1 class="header-text">Anmeldung zur Ausbildung als MFA / ZFA an der Rahel-Hirsch-Schule</h1>
+            </header>
+        <main>
+        <div class="homepage-container">
+        <button class="login-btn">
+            Anmelden
+        </button>
+            </div>
+             
+        </main>
+
+  ';
+    }
+}
