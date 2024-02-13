80085df9df6f1fccd78b6bc703076c10e253cc55
MaximilianB97 <146432049+MaximilianB97@users.noreply.github.com>
2024-02-13 11:02:42 +0100

    [ADD] init.sql for apprenticeship, occupation name, school day options and chosen option

---
+++ migrations/init_chosenOption.sql
@@ -0,0 +1,4 @@
+CREATE IF NOT EXISTS TABLE StudentRegistration.chosenOption (
+    id_chosenOption INT PRIMARY KEY AUTO_INCREMENT, 
+    chosen_option INT(1), -- chosen option/1. Wahl oder 2. Wahl
+);
---
+++ migrations/init_occupationName.sql
@@ -0,0 +1,4 @@
+CREATE IF NOT EXISTS TABLE StudentRegistration.occupationName (
+    id_occupationName INT PRIMARY KEY AUTO_INCREMENT, 
+    occupation_name VARCHAR(255), -- occupation name/Berufsname
+);
