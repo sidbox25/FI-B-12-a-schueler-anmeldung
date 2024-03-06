07bf0b924f4639baa3ddf8de06a658e877d2728e
Mandy Wockenfuß <86933191+mandywocky@users.noreply.github.com>
2024-03-06 13:02:42 +0100

    Seitenaufteilung für Tabelle

--- src/StudentRegistration/View/StudentSchoolVisitsView.php
+++ src/StudentRegistration/View/StudentSchoolVisitsView.php
@@ -14,7 +14,11 @@ class StudentSchoolVisitsView
     <p id = "bisheriger_schulbesuch">Bisherige Schulbesuche</p>
 
     <div class = "container">
-        <label for="schulabschluss">Schulabschluss:</label>
+        <div class = "containerWhite">
+            <label for="schulabschluss">Schulabschluss:</label>
+            <p style="margin-top: 5px; font-style: italic; font-size: 9pt; color: grey">Wählen Sie hier nur Ihren zuletzt erreichten Schulabschluss aus.</p>
+        </div>
+        <div>
         <select id="schulabschluss" name="schulabschluss">
             <option value="berufsbildungsreife">Berufsbildungsreife</option>
             <option value="erweiterte_berufsbildungsreife">Erweiterte Berufsbildungsreife</option>
@@ -23,45 +27,54 @@ class StudentSchoolVisitsView
             <option value="fachabitur_schriftlich">Fachabitur schriftl. Teil</option>
             <option value="ohne">Ohne</option>
         </select>
-            <p style="margin-top: 10px; font-style: italic;">Wählen Sie hier nur Ihren zuletzt erreichten Schulabschluss aus.</p>
+        </div>
     </div>
 
     <div class = "container">
-        <label for="last_school">Zuletzt besuchte Schule:</label>
-        <input type="text" id="last_school" name="last_school" title="Zuletzt besuchte Schule">
+        <label class = "containerWhite", for="last_school">Zuletzt besuchte Schule:</label>
+        <div>
+            <input type="text" id="last_school" name="last_school" title="Zuletzt besuchte Schule">
+        </div>
     </div>
 
     <div class = "container">
-        <label for="completion_year">Abschluss-/Abgangsjahr:</label>
-        <input type="text" id="completion_year" name="completion_year" title="Abschluss-/Abgangsjahr">
+        <label class = "containerWhite", for="completion_year">Abschluss-/Abgangsjahr:</label>
+        <div>
+            <input type="text" id="completion_year" name="completion_year" title="Abschluss-/Abgangsjahr">
+        </div>
     </div>
 
     <div class = "container">
-        <label for="abgeschlossener_lehrgang">Abgeschlossener Lehrgang:</label>
-        <select id="abgeschlossener_lehrgang" name="abgeschlossener_lehrgang">
-            <option value="IBA">IBA</option>
-            <option value="BQL">BQL</option>
-            <option value="BV">BV</option>
-            <option value="Willkommensklasse">Willkommensklasse</option>
-            <option value="Keinen">Keinen</option>
-        </select>
+        <label class = "containerWhite", for="abgeschlossener_lehrgang">Abgeschlossener Lehrgang:</label>
+        <div>
+            <select id="abgeschlossener_lehrgang" name="abgeschlossener_lehrgang">
+                <option value="IBA">IBA</option>
+                <option value="BQL">BQL</option>
+                <option value="BV">BV</option>
+                <option value="Willkommensklasse">Willkommensklasse</option>
+                <option value="Keinen">Keinen</option>
+            </select>
+        </div>
     </div>
 
     <div class = "container">
-        <p>Neu an der Rahel-Hirsch-Schule?</p>
-        <label>
-            <input type="checkbox" name="neu_an_der_schule" value="ja"> Ja, ich bin neu an der Rahel-Hirsch-Schule
-        </label>
+        <div class = "containerWhite">
+            <p>Neu an der Rahel-Hirsch-Schule?</p>
+        </div>
+        <div>
+            <label>
+                <input type="checkbox" name="neu_an_der_schule" value="ja"> Ja, ich bin neu an der Rahel-Hirsch-Schule
+            </label>
 
-        <label>
-            <input type="checkbox" name="neu_an_der_schule" value="war_schueler"> Ich war bereits Schüler*in der Rahel-Hirsch-Schule
-        </label>
-
-    <label>
-        <input type="checkbox" name="neu_an_der_schule" value="aktuell_schueler"> Ich bin aktuell Schüler*in der Rahel-Hirsch-Schule
-    </label>
-    <a class="wieter-schoolvisit-btn" href="/schultage">Weiter</a>
+            <label>
+                <input type="checkbox" name="neu_an_der_schule" value="war_schueler"> Ich war bereits Schüler*in der Rahel-Hirsch-Schule
+            </label>
+            <label>
+                <input type="checkbox" name="neu_an_der_schule" value="aktuell_schueler"> Ich bin aktuell Schüler*in der Rahel-Hirsch-Schule
+            </label>
+        </div>
     </div>
+    <a class="weiter-schoolvisit-btn" href="/schultage">Weiter</a>
 </form>
 </body>
 
@@ -71,32 +84,24 @@ class StudentSchoolVisitsView
         color: white;
     }
 
-    .wrapper {
+    .container {
         display: grid;
-        /*grid-template-columns: [first] 30% [second] 70%;*/
-        rid-template-columns: [first] 40px [line2] 50px [line3] auto [col4-start] 50px [five] 40px [end];
-        grid-template-rows: [row1-start] 25% [row1-end] 100px [third-line] auto [last-line];
-    }
-
-    .container1 {
-        display: grid;
-        grid-template-columns: auto;
+        grid-template-columns: [left] 10fr [middle1] 15fr [middle2] 15fr [right];
+        grid-template-rows: auto;
         grid-column-start: 1;
         grid-column-end: 2;
-    }
-
-    /*.container {
-        display: grid;
-        /*grid-gap: 10px;*/
-        /*grid-template-columns: [left] 10fr [middle1] 10fr [middle2] 10fr [right];*/
-        grid-template-columns: auto auto auto auto;
+        grid-gap: 10px;
         border: 1px solid;
         border-color: white;
         width: 90%;
-        padding: 4px;
-    }*/
+        padding: 1px;
+    }
+
+    .containerWhite {
+        background-color: white;
+    }
+
 </style>
 ';
     }
-
 }
