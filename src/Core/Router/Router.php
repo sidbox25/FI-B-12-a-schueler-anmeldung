7772404b101c79af2df99b4f1b8370e18bc5ce10
Oliver Vier <119545999+OliverVier@users.noreply.github.com>
2024-04-30 10:00:02 +0200

    Added router class, changed routing location from index to Router class

---
+++ src/Core/Router/Router.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace src\Core\Router;
+use src\GeschafftTab\Controller\GeschafftTabController;
+use src\HomePage\HomePageController\HomePageController;
+use src\StudentPersonalData\Controller\StudentPersonalDataController;
+use src\StudentRegistration\Controller\StudentRegistrationApprenticeController;
+use src\StudentRegistration\Controller\StudentRegistrationController;
+use src\StudentRegistration\Controller\StudentSchoolVisitsController;
+//use src\UploadPdf\Controller\StudentRegistrationApprenticeshipController;
+use src\StudentResidence\Controller\StudentResidenceController;
+use src\StudentAge\Controller\StudentAgeController;
+use src\Core\Connector;
+
+class Router 
+{
+    private static $instance = null;
+
+    public static function getInstance() 
+    {
+        if (self::$instance == null) {
+            self::$instance = new Router();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * when called, route method calls the contructor of the controller
+     * responsible for the given parameter $uri
+     * @param uri String
+     **/
+    public function route($request_uri)
+    {
+
+        $uri = parse_url($_SERVER['REQUEST_URI'])["path"];
+
+        switch ($uri) {
+
+            case '/':
+                $homePageController = new HomePageController();
+                $homePageController->showAction();
+                break;
+            
+            case '/schulbesuch':
+                $studentSchoolVisitsController = new StudentSchoolVisitsController();
+                $studentSchoolVisitsController->showStudentSchoolVisitsAction();
+                break;
+
+            case '/schultage':
+                $studentSchoolVisitsController = new StudentRegistrationApprenticeController();
+                $studentSchoolVisitsController->studentRegistrationApprenticeViewAction();    
+                break;
+
+            case '/persoenliche_daten':
+                $studentPersonalDataController = new StudentPersonalDataController();
+                $studentPersonalDataController->studentPersonalDataViewAction();    
+                break;
+
+            case '/wohnort':
+                $studentResidenceController = new StudentResidenceController();
+                $studentResidenceController->studentResidenceViewAction();    
+                break;
+
+            case '/alter':
+                $studentAgeController = new StudentAgeController();
+                $studentAgeController->showViewAction();    
+                break;
+
+            case '/geschafft':
+                $geschafftTabController = new GeschafftTabController();
+                $geschafftTabController->showGeschafftTabViewAction();
+                break;
+
+            default:
+                $homePageController = new HomePageController();
+                $homePageController->showAction();
+                break;
+        }
+    }
+}
+
+?>
